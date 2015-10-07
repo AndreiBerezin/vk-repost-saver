1d29b67b5c6fbde3fea06854a79b6b0bf6037c7d
Andrei Berezin <yago.spb@gmail.com>
2015-10-07 10:51:21 +0300

    test

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import config
-import vk
+from vk.api import API, Session
+from vk.mixins import AuthMixin
 import os
 
 def main():
-    print 'hello world!'
+    auth = AuthMixin(app_id=config.VK_APP_ID, user_login=config.VK_LOGIN, user_password=config.VK_PASSWORD)
+    #auth = InteractiveMixin()
+    #auth_session = APISession(app_id=config.VK_APP_ID, user_login=config.VK_LOGIN, user_password=config.VK_PASSWORD)
+    access_token, _ = auth.get_access_token()
+    session = Session(access_token=access_token)
+    vk_api = API(session, lang='ru')
+    print vk_api.users.get(user_ids=1)
+
 
 main()
